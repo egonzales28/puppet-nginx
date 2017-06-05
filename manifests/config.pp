eae3b13b4081286c6a49e8666e56783dbc7c3bea
egonzales28 <fgonzales@forsythe.com>
2017-06-05 01:47:14 -0400

    fixed template

--- manifests/config.pp
+++ manifests/config.pp
@@ -20,7 +20,7 @@ class nginx::config (
     mode    => $mode,
     owner   => $owner,
     group   => $group,
-    content => template("${module_name}/nginx.conf.erb"),
+    content => template("${module_name}/conf.d/nginx.conf.erb"),
   }
 
   file { $log_dir: 
